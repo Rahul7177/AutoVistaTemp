32a9209700ce490170771a8566fc15b6c6e6acd5
Rahul Raj <rahulr7177@gmail.com>
2023-11-05 02:44:20 +0530

    Add existing project files to Git

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-
+import Brands from '../components/Brands'
 const Homepage = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Brands/>
     </div>
   )
 }
